275eab8952575b271818570dc26ad73d027450df
kimaiyo <meshackkimaiyo5@gmail.com>
2025-10-07 11:54:59 +0300

    feat: initialize kfexpress project with TypeScript, Express, and necessary routes for practise

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoute from './routes/authRoute';
+
+import productRoute from './routes/productRoute';
+
+dotenv.config({ path: './.env' });
+
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.use('/api/v1/auth', authRoute);
+
+
+app.use('/api/v1/products', productRoute);
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
